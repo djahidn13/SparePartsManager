abf1c8344299ede3a66bfe3644a9ec9eb420bfe0
djahidn13 <djahidn@googlemail.com>
2025-08-13 14:27:05 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css"
 export const metadata: Metadata = {
   title: "AutoParts Manager",
   description: "Created with v0",
-  generator: "v0.dev",
+  generator: "Auto Rechange",
+  robots: "noindex, nofollow",
 }
 
 export default function RootLayout({
